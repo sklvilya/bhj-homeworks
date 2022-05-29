cadb79f9c4cd859c52c787da39cf4d442ab2d88a
sklvilya <sklvilyaomsk@gmail.com>
2022-05-29 18:29:40 +0600

    Обновлен файл task.js для задачи №3

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,90 +1,70 @@
-const fontSizeButtons = Array.from(document.querySelectorAll(".font-size"));
-const book = document.getElementById('book');
+const book = document.querySelector(".book");
+const textSize = document.querySelectorAll(".font-size");
+const textColor = document.querySelectorAll('[data-text-color]');
+const backgroundColor = document.querySelectorAll('[data-bg-color]');
 
-const colorButtons = Array.from(document.querySelectorAll('.book__control_color > .color')) 
-const backgroundColorButtons = Array.from(document.querySelectorAll('.book__control_background > .color'))
-
-function deActivateButtons() {
-    for (const btn of fontSizeButtons) {
-        btn.classList.remove('font-size_active');
+textSize.forEach((e) => {
+  e.onclick = function () {
+    textSize.forEach((e) => {
+      e.classList.remove("font-size_active");
+    });
+    e.classList.add("font-size_active");
+    if (e.classList.contains('font-size_small')) {
+      book.classList.remove('book_fs-big')
+      book.classList.add('book_fs-small')
+    } else if (e.classList.contains('font-size_big')) {
+      book.classList.remove('book_fs-small')
+      book.classList.add('book_fs-big')
+    } else {
+      book.classList.remove('book_fs-small')
+      book.classList.remove('book_fs-big')
     }
-    book.className = 'book';
-}
-
-fontSizeButtons[0].addEventListener('click', function(event) {
-    deActivateButtons();
-    fontSizeButtons[0].classList.add('font-size_active');
-    book.classList.add('book_fs-small');
-    event.preventDefault();
+    return false;
+  };
 });
 
-fontSizeButtons[1].addEventListener('click', function(event) {
-    deActivateButtons();
-    fontSizeButtons[1].classList.add('font-size_active');
-    book.className = 'book';
-    event.preventDefault();
-});
-
-fontSizeButtons[2].addEventListener('click', function(event) {
-    deActivateButtons();
-    fontSizeButtons[2].classList.add('font-size_active');
-    book.classList.add('book_fs-big');
-    event.preventDefault();
-});
-
-function deActivateButtonsColor() {
-    for (const btn of colorButtons) {
-        btn.classList.remove('color_active');
+textColor.forEach((e) => {
+  e.onclick = function () {
+    textColor.forEach((e) => {
+      e.classList.remove("color_active");
+    });
+    e.classList.add("color_active");
+    if (e.classList.contains('text_color_black')) {
+      book.classList.remove('book_color-gray')
+      book.classList.remove('book_color-whitesmoke')
+      book.classList.add('book_color-black')
+    } else if (e.classList.contains('text_color_gray')) {
+      book.classList.remove('book_color-whitesmoke')
+      book.classList.remove('book_color-black')
+      book.classList.add('book_color-gray')
+    } else {
+      book.classList.remove('book_color-black')
+      book.classList.remove('book_color-gray')
+      book.classList.add('book_color-whitesmoke')
     }
-    // book.className = 'book';
-}
-
-colorButtons[0].addEventListener('click', function(event){
-    deActivateButtonsColor();
-    colorButtons[0].classList.add('color_active');
-    book.classList.add('book_color-black');
-    event.preventDefault();
+    return false;
+  };
 });
 
-colorButtons[1].addEventListener('click', function(event){
-    deActivateButtonsColor();
-    colorButtons[1].classList.add('color_active');
-    book.classList.add('book_color-gray');
-    event.preventDefault();
-});
-
-colorButtons[2].addEventListener('click', function(event){
-    deActivateButtonsColor();
-    colorButtons[2].classList.add('color_active');
-    book.classList.add('book_color-whitesmoke');
-    event.preventDefault();
-});
-
-
-function deActivateButtonsBackGroundColor() {
-    for (const btn of backgroundColorButtons) {
-        btn.classList.remove('color_active');
+backgroundColor.forEach((e) => {
+  e.onclick = function () {
+    backgroundColor.forEach((e) => {
+      e.classList.remove("color_active");
+    });
+    e.classList.add("color_active");
+    if (e.classList.contains('bg_color_black')) {
+      book.classList.remove('book_bg-gray')
+      book.classList.remove('book_bg-whitesmoke')
+      book.classList.add('book_bg-black')
+    } else if (e.classList.contains('bg_color_gray')) {
+      book.classList.remove('book_bg-whitesmoke')
+      book.classList.remove('book_bg-black')
+      book.classList.add('book_bg-gray')
+    } else {
+      book.classList.remove('book_bg-black')
+      book.classList.remove('book_bg-gray')
+      book.classList.add('book_bg-whitesmoke')
     }
-    // book.className = 'book';
-}
-
-backgroundColorButtons[0].addEventListener('click', function(event){
-    deActivateButtonsBackGroundColor();
-    backgroundColorButtons[0].classList.add('color_active');
-    book.classList.add('text_color_black');
-    event.preventDefault();
-});
-
-backgroundColorButtons[1].addEventListener('click', function(event){
-    deActivateButtonsBackGroundColor();
-    backgroundColorButtons[1].classList.add('color_active');
-    book.classList.add('text_color_gray');
-    event.preventDefault();
-});
-
-backgroundColorButtons[2].addEventListener('click', function(event){
-    deActivateButtonsBackGroundColor();
-    backgroundColorButtons[2].classList.add('color_active');
-    book.classList.add('text_color_whitesmoke');
-    event.preventDefault();
+    return false;
+  };
 });
